4981c6b77be0f19b4bfed928c20a283e1945166a
Lavnia005 <laviniaandrade005@gmail.com>
2024-09-02 09:34:16 -0300

    mudanca apenas na questao de após o login, o nome e a opção se sair aparecer.

--- finexo-html/usuario/indexg.php
+++ finexo-html/usuario/indexg.php
@@ -58,25 +58,25 @@ session_start();
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav  ">
-              <li class="nav-item active">
+              <li class="nav-item">
                 <a class="nav-link" href="../index.html">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="../team.html">Equipe</a>
-              </li>
-              <li class="nav-item">
+              </li>             
               <?php
                 if (isset($_SESSION['nome_usuario'])) {
                   $nome_usuario = $_SESSION['nome_usuario'];
-                  print "<p>Olá $nome_usuario! <a class='nav-link' href='logout.php'><i class='fa fa-user'></i>Sair</a></a>";
-
+                 
+                  print "<li class='nav-item'><a class='nav-link login-user' href='perfil.php'><i class='fa fa-user'></i>$nome_usuario</a></li>";
+                  print "<li class='nav-item'><a class='nav-link' href='logout.php'><i class='fa fa-sign-out'></i>Sair</a></li>";
+                 
                 } else {
                   ?>
-                    <a class="nav-link" href="#"> <i class="fa fa-user" aria-hidden="true"></i>Login</a>
+                    <li class="nav-item"><a class="nav-link" href="#"> <i class="fa fa-user" aria-hidden="true"></i>Login</a></li>
                   <?php                 
                 }
-              ?>
-              </li>
+              ?>              
               <form class="form-inline">
                 <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
                   <i class="fa fa-search" aria-hidden="true"></i>
