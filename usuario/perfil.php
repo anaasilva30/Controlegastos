bccfab5246d48870f3c98bac66c784fae54e6c0c
Lavnia005 <laviniaandrade005@gmail.com>
2024-09-23 08:38:44 -0300

    mudanca perfil

--- usuario/perfil.php
+++ usuario/perfil.php
@@ -1,8 +1,6 @@
-
 <?php
 // Inicia a sessao.
 session_start();
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,27 +8,27 @@ session_start();
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <link rel="shortcut icon" href="images/favicon.png" type="">
+  <link rel="shortcut icon" href="../images/favicon.png" type="">
 
-  <title> SaldoPrático</title>
+  <title> Controle de Gastos </title>
 
   <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
 
   <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+  <link href="../https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
 
   <!--owl slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <link rel="stylesheet" type="text/css" href="../https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
 
   <!-- font awesome style -->
-  <link href="css/font-awesome.min.css" rel="stylesheet" />
+  <link href="../css/font-awesome.min.css" rel="stylesheet" />
 
   <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" />
+  <link href="../css/style.css" rel="stylesheet" />
 
   <!-- responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
+  <link href="../css/responsive.css" rel="stylesheet" />
 
 </head>
 
@@ -40,7 +38,7 @@ session_start();
 
     <div class="hero_bg_box">
       <div class="bg_img_box">
-        <img src="images/hero-bg.png" alt="">
+        <img src="../images/hero-bg.png" alt="">
       </div>
     </div>
 
@@ -48,9 +46,9 @@ session_start();
     <header class="header_section">
       <div class="container-fluid">
         <nav class="navbar navbar-expand-lg custom_nav-container ">
-          <a class="navbar-brand" href="index.php">
+          <a class="navbar-brand" href="index.html">
             <span>
-            SaldoPrático
+              ClearCash
             </span>
           </a>
 
@@ -60,28 +58,25 @@ session_start();
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav  ">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <li class="nav-item">
+                <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="team.php">Equipe</a>
-              </li> 
-   
-
+                <a class="nav-link" href="../team.php">Equipe</a>
+              </li>             
               <?php
                 if (isset($_SESSION['nome_usuario'])) {
                   $nome_usuario = $_SESSION['nome_usuario'];
                  
-                  print "<li class='nav-item'><a class='nav-link login-user' href='gastos/index.php'><i class='fa fa-dollar'></i>Gastos</a></li>";
-                  print "<li class='nav-item'><a class='nav-link login-user' href='usuario/perfil.php'><i class='fa fa-user'></i>$nome_usuario</a></li>";
-                  print "<li class='nav-item'><a class='nav-link' href='usuario/logout.php'><i class='fa fa-sign-out'></i>Sair</a></li>";
+                  print "<li class='nav-item'><a class='nav-link login-user' href='perfil.php'><i class='fa fa-user'></i>$nome_usuario</a></li>";
+                  print "<li class='nav-item'><a class='nav-link' href='../usuario/logout.php'><i class='fa fa-sign-out'></i>Sair</a></li>";
                  
                 } else {
                   ?>
-                    <li class="nav-item"><a class="nav-link" href="usuario/indexg.php"> <i class="fa fa-user" aria-hidden="true"></i>Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"> <i class="fa fa-user" aria-hidden="true"></i>Login</a></li>
                   <?php                 
                 }
-              ?> 
+              ?>              
               <form class="form-inline">
                 <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
                   <i class="fa fa-search" aria-hidden="true"></i>
@@ -98,27 +93,31 @@ session_start();
     <div class="container  ">
       <div class="heading_container heading_center">
         <h2>
-          Simplifique sua gestão <span>financeira</span>
+          Preencha seus dados e venha fazer parte do<span>ClearCash</span>
         </h2>
+        <p>
+          Centralize suas despesas e recebimentos e facilite gestão financeira.
+        </p>
       </div>
       <div class="row">
         <div class="col-md-6 ">
           <div class="img-box">
-            <img src="images/about-img.png" alt="">
+            <img src="../images/about-img.png" alt="">
           </div>
         </div>
         <div class="col-md-6">
           <div class="detail-box">
             <h3>
-              Sobre nós 
+              Login do usuário
             </h3>
-            <p>
-              No SaldoPrático, entendemos que gerenciar suas finanças pode ser um desafio, mas estamos aqui para tornar isso mais simples e eficiente. Nosso objetivo é fornecer a você as ferramentas e a clareza necessárias para manter suas despesas sob controle e alcançar suas metas financeiras com confiança.
-            </p>
-            <p>
-              Inicie sua jornada rumo a uma gestão financeira mais estruturada e serena. Comece a administrar seus gastos com maior precisão e transparência. O caminho para o seu sucesso financeiro começa agora!
-            </p>
-            <a href='usuario/cadastra.html'>Cadastre-se.</a>
+            <form method="post" action="confirmalogin.php">
+            <label>CPF: </label>
+            <input name='cpf_usuario'><br>
+            <label>Senha: </label>
+            <input type='password' name='senha_usuario' /><br>
+            <button type='submit'>Logar</button><br>
+            <a href='cadastra.html'>Não tem login? Cadastre-se.</a>
+        </form>
           </div>
         </div>
       </div>
@@ -139,7 +138,7 @@ session_start();
               <a href="">
                 <i class="fa fa-envelope" aria-hidden="true"></i>
                 <span>
-                  saldopraticooficial@gmail.com
+                  clearcashoficial@gmail.com
                 </span>
               </a>
             </div>
@@ -199,8 +198,8 @@ session_start();
   <section class="footer_section">
     <div class="container">
       <p>
-        &copy; <span id="displayYear"></span>
-        <a>Equipe SaldoPrático</a>
+        &copy; <span id="displayYear"></span> 
+        <a>Equipe ClearCash</a>
       </p>
     </div>
   </section>
